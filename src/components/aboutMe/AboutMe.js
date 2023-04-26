59ead08f9ab9ba34d37f5f95a1411d395221bb62
Teepee78 <eniolatoluwalase78@gmail.com>
2023-04-26 23:28:19 +0100

    Update projects

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -6,98 +6,99 @@ import greenDir from "../../assets/svg/greendiricon.svg";
 import mailIcon from "../../assets/svg/mail-icon.svg";
 
 function AboutMe() {
-  return (
-    <div id="aboutMe" className="aboutMe">
-      <div className="about">
-        <p>about me</p>
-      </div>
+	return (
+		<div id="aboutMe" className="aboutMe">
+			<div className="about">
+				<p>about me</p>
+			</div>
 
-      <div className="aboutTable">
-        <div className="contents">
-          <div className="about-table-left">
-            <div className="personal-info-title">
-              <img src={downArrow} alt="down arrow"></img>
-              <p>personal-info</p>
-            </div>
+			<div className="aboutTable">
+				<div className="contents">
+					<div className="about-table-left">
+						<div className="personal-info-title">
+							<img src={downArrow} alt="down arrow"></img>
+							<p>personal-info</p>
+						</div>
 
-            <div className="tableofcontent">
-              <div className="bioTable">
-                <img src={rightArrow} alt="right arrow"></img>
-                <img src={purpleDir} alt="purple directory icon"></img>
-                <p>bio</p>
-              </div>
-              <div className="interestsTable">
-                <img src={rightArrow} alt="right arrow"></img>
-                <img src={greenDir} alt="green directory icon"></img>
-                <p>interests</p>
-              </div>
-            </div>
+						<div className="tableofcontent">
+							<div className="bioTable">
+								<img src={rightArrow} alt="right arrow"></img>
+								<img src={purpleDir} alt="purple directory icon"></img>
+								<p>bio</p>
+							</div>
+							<div className="interestsTable">
+								<img src={rightArrow} alt="right arrow"></img>
+								<img src={greenDir} alt="green directory icon"></img>
+								<p>interests</p>
+							</div>
+						</div>
 
-            <div className="contact-big">
-              <img src={downArrow} alt="down arrow"></img>
-              <p>contact-me</p>
-            </div>
-            <div className="contact-email-big">
-              <img src={mailIcon} alt="mail icon"></img>
-              <p>eniolatoluwalase78@gmail.com</p>
-            </div>
-          </div>
+						<div className="contact-big">
+							<img src={downArrow} alt="down arrow"></img>
+							<p>contact-me</p>
+						</div>
+						<div className="contact-email-big">
+							<img src={mailIcon} alt="mail icon"></img>
+							<p>eniolatoluwalase78@gmail.com</p>
+						</div>
+					</div>
 
-          <div className="personal-info">
-            <div className="bio">
-              <div className="bioHeader">
-                <p>// personal-info</p>
-                <p>/ bio</p>
-                <p>personal-info</p>
-                <div className="border-left"></div>
-              </div>
-              <p className="bioContent">
-                I am a software developer with one year of experience in a
-                variety of programming languages and frameworks.
-                <br />
-                <br />
-                My expertise includes Python, NodeJS, TypeScript, C, C++, Bash, SQL, HTML,
-                CSS and Git tools. I have experience working with frameworks
-                such as FastAPI, ExpressJS, React, Django, Flask, and Bootstrap.
-                <br />
-                <br />
-                I am well-versed in using Git for version control and
-                collaboration.
-                <br />
-                <br />I am a quick learner and am always looking to expand my
-                skills and knowledge in the field. I am excited to continue to
-                develop my skills and work on challenging and engaging projects.
-              </p>
-            </div>
+					<div className="personal-info">
+						<div className="bio">
+							<div className="bioHeader">
+								<p>// personal-info</p>
+								<p>/ bio</p>
+								<p>personal-info</p>
+								<div className="border-left"></div>
+							</div>
+							<p className="bioContent">
+								I am a software developer with one year of experience in a
+								variety of programming languages and frameworks.
+								<br />
+								<br />
+								My expertise includes Python, NodeJS, TypeScript, C, C++, Bash,
+								SQL, HTML, CSS and Git tools. I have experience working with
+								frameworks such as FastAPI, ExpressJS, React, Django, Flask, and
+								Bootstrap.
+								<br />
+								<br />
+								I am well-versed in using Git for version control and
+								collaboration.
+								<br />
+								<br />I am a quick learner and am always looking to expand my
+								skills and knowledge in the field. I am excited to continue to
+								develop my skills and work on challenging and engaging projects.
+							</p>
+						</div>
 
-            <div className="interests">
-              <div className="interestsHeader">
-                <p>// personal-info</p>
-                <p>/ interests</p>
-                <p>interests</p>
-                <div className="border-left"></div>
-              </div>
-              <div className="interestsContent">
-                <p className="orange-text">writing code</p>
-                <p className="grey-text">watching movies</p>
-                <p className="orange-text">playing games</p>
-                <p className="grey-text">making music</p>
-              </div>
-            </div>
+						<div className="interests">
+							<div className="interestsHeader">
+								<p>// personal-info</p>
+								<p>/ interests</p>
+								<p>interests</p>
+								<div className="border-left"></div>
+							</div>
+							<div className="interestsContent">
+								<p className="orange-text">writing code</p>
+								<p className="grey-text">watching movies</p>
+								<p className="orange-text">playing games</p>
+								<p className="grey-text">making music</p>
+							</div>
+						</div>
 
-            <div className="personal-info-title contact-small">
-              <img src={downArrow} alt="down arrow"></img>
-              <p>contact-me</p>
-            </div>
-            <div className="contact-email contact-small">
-              <img src={mailIcon} alt="mail icon"></img>
-              <p>eniolatoluwalase78@gmail.com</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+						<div className="personal-info-title contact-small">
+							<img src={downArrow} alt="down arrow"></img>
+							<p>contact-me</p>
+						</div>
+						<div className="contact-email contact-small">
+							<img src={mailIcon} alt="mail icon"></img>
+							<p>eniolatoluwalase78@gmail.com</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default AboutMe;
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,81 +3,81 @@ import rec1 from "../../assets/svg/Rectangle 42.svg";
 import rec2 from "../../assets/svg/Rectangle 43.svg";
 
 function Cards(props) {
-  return (
-    <div id="projects" className="project">
-      <div id="project-title">
-        <p className="project-title">{props.title}</p>
-        <p className="project-subtitle">{props.lang}</p>
-      </div>
+	return (
+		<div id="projects" className="project">
+			<div id="project-title">
+				<p className="project-title">{props.title}</p>
+				<p className="project-subtitle">{props.lang}</p>
+			</div>
 
-      <div className="project-card">
-        <img src={props.img} alt="placeholder" />
+			<div className="project-card">
+				<img src={props.img} alt="placeholder" />
 
-        <p className="project-description">{props.desc}</p>
+				<p className="project-description">{props.desc}</p>
 
-        <div className="view-project">
-          <a href={props.url} target="_blank" rel="noreferrer">
-            view-project
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+				<div className="view-project">
+					<a href={props.url} target="_blank" rel="noreferrer">
+						view-project
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 function Projects() {
-  return (
-    <div className="Projects">
-      <div className="projects-title">
-        <p>projects</p>
-      </div>
+	return (
+		<div className="Projects">
+			<div className="projects-title">
+				<p>projects</p>
+			</div>
 
-      <div className="projects-cards">
-        <Cards
-          title="ChowAI"
-          lang="// python(fastAPI)"
-          img={rec1}
-          desc="ChowAi is an AI-powered application that provides a wide variety of recipes for African dishes and also suggests dishes based on user-inputted ingredients."
-          url="https://chowai.com"
-        />
-        <Cards
-          title="StreetRates"
-          lang="// python(fastAPI)"
-          img={rec2}
-          desc="StreetRates is an application that allows users to convert currencies using the parallel bank rate. It also provides users with additional information about currency fluctuations and helps them to make informed decisions about international transactions."
-          url="https://streetrates.info"
-        />
-        {/* <Cards
+			<div className="projects-cards">
+				<Cards
+					title="Mumble"
+					lang="// Typescript(Next.JS/Node.JS)"
+					img={rec1}
+					desc="Mumble is a digital marketplace that connects music producers, beat makers and sound engineers to their desired clients."
+					url="https://mumble-client.vercel.app"
+				/>
+				<Cards
+					title="Innov8IQ"
+					lang="// Typescript(Node.JS))"
+					img={rec2}
+					desc="Innov8IQ is an application that helps UI/UX designers write compelling case studies by providing existing customizable templates and arming them with resources to easily make one from scratch"
+					url="https://innov8iq.com"
+				/>
+				<Cards
+					title="ChowAI"
+					lang="// python(fastAPI)"
+					img={rec1}
+					desc="ChowAi is an AI-powered application that provides a wide variety of recipes for African dishes and also suggests dishes based on user-inputted ingredients."
+					url="https://chowai.com"
+				/>
+				<Cards
+					title="StreetRates"
+					lang="// python(fastAPI)"
+					img={rec2}
+					desc="StreetRates is an application that allows users to convert currencies using the parallel bank rate. It also provides users with additional information about currency fluctuations and helps them to make informed decisions about international transactions."
+					url="https://streetrates.info"
+				/>
+				{/* <Cards
           title="ChowAI"
           lang="// python(fastAPI)"
           img={rec1}
           desc="ChowAi is an AI-powered application that provides a wide variety of recipes for African dishes and also suggests dishes based on user-inputted ingredients."
           url="https://chowai.com"
         /> */}
-        {/* <Cards
-          title="StreetRates"
-          lang="// python(fastAPI)"
-          img={rec2}
-          desc="StreetRates is an application that allows users to convert currencies using the parallel bank rate. It also provides users with additional information about currency fluctuations and helps them to make informed decisions about international transactions."
-          url="https://streetrates.info"
-        /> */}
-        {/* <Cards
-          title="ChowAI"
-          lang="// python(fastAPI)"
-          img={rec1}
-          desc="ChowAi is an AI-powered application that provides a wide variety of recipes for African dishes and also suggests dishes based on user-inputted ingredients."
-          url="https://chowai.com"
-        /> */}
-        {/* <Cards
+				{/* <Cards
           title="StreetRates"
           lang="// python (fastAPI)"
           img={rec2}
           desc="StreetRates is an application that allows users to convert currencies using the parallel bank rate. It also provides users with additional information about currency fluctuations and helps them to make informed decisions about international transactions."
           url="https://streetrates.info"
         /> */}
-      </div>
-    </div>
-  );
+			</div>
+		</div>
+	);
 }
 
 export default Projects;
